3c0e482403bd76dad0ba754354d287173bb31f18
wzqhbkjdx <wzqhbkjdx@126.com>
2017-01-17 18:05:36 +0800

    api

--- www/test.py
+++ www/test.py
@@ -7,7 +7,9 @@ from models import User, Blog, Comment
 
 #def ormTest():
 
-u = User(name='Test', email='test@example.com',passwd='1234567890', image='about:blank')
+#u = User(name='Test', email='test@example.com',passwd='1234567890', image='about:blank')
+u2 = User(name = 'Hello', email='hello@backyard.com', passwd='kdlskdls', image='about:blank')
 loop = asyncio.get_event_loop()
 loop.run_until_complete(orm.get_pool(loop))
-loop.run_until_complete(u.save())
+#loop.run_until_complete(u.save())
+loop.run_until_complete(u2.save())
